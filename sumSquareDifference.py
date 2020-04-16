ff337f0c88c93794ea6d3ff38251b885d1c20e53
katerina-vasileiou <katerinavas96@gmail.com>
2020-04-17 00:52:57 +0300

    Add files via upload

---
+++ sumSquareDifference.py
@@ -0,0 +1,33 @@
+import sys
+from math import pow
+
+sumSqr = 0
+sqrSum = 0
+
+n = int(sys.argv[1])
+sqr = []
+numbers = list([])
+for i in range(1, n + 1):
+    numbers.append(i)
+
+    sumDigits = sum(numbers)
+    sqrSum = int(pow(sumDigits, 2))
+
+
+    sqr.append(int(pow(numbers[i-1], 2)))
+    sumSqr = sum(sqr)
+
+diff = sqrSum - sumSqr
+print(sumSqr)
+print(sqrSum)
+print("the difference is: ", diff)
+
+
+
+
+
+
+
+
+
+
