e8907342cc5d44a7b82e442b765540f75f997ff0
katerina-vasileiou <katerinavas96@gmail.com>
2020-04-17 00:56:13 +0300

    Update sumSquareDifference.py

--- sumSquareDifference.py
+++ sumSquareDifference.py
@@ -4,15 +4,15 @@ from math import pow
 sumSqr = 0
 sqrSum = 0
 
-n = int(sys.argv[1])
+n = int(sys.argv[1])    #how many digits do you want? (from one to n, give n)
+
 sqr = []
 numbers = list([])
+
 for i in range(1, n + 1):
     numbers.append(i)
-
     sumDigits = sum(numbers)
     sqrSum = int(pow(sumDigits, 2))
-
 
     sqr.append(int(pow(numbers[i-1], 2)))
     sumSqr = sum(sqr)
