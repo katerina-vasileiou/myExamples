07ff9ede64e8516b7c72a2cfc86a74a0a7f61bfd
katerina-vasileiou <katerinavas96@gmail.com>
2020-04-17 01:01:03 +0300

    Update sumSquareDifference.py

--- sumSquareDifference.py
+++ sumSquareDifference.py
@@ -4,7 +4,7 @@ from math import pow
 sumSqr = 0
 sqrSum = 0
 
-n = int(sys.argv[1])    #how many digits do you want? (from one to n, give n)
+n = int(sys.argv[1])    #how many first digits do you want? (from 1 to n, give n)
 
 sqr = []
 numbers = list([])
@@ -18,8 +18,8 @@ for i in range(1, n + 1):
     sumSqr = sum(sqr)
 
 diff = sqrSum - sumSqr
-print(sumSqr)
-print(sqrSum)
+print(sumSqr)   #gives the sum of 1^2 + 2^2 +...+n^2
+print(sqrSum)   #gives the square of sum (1+2+3+...+n)^2
 print("the difference is: ", diff)
 
 
